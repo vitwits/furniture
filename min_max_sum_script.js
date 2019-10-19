fc310ab403ee4d558480bdbb943640f9663584f2
Vitwits <vitwits@gmail.com>
2019-10-19 09:23:43 +0300

    fixed js file

--- min_max_sum_script.js
+++ min_max_sum_script.js
@@ -1,44 +1,38 @@
-///////////MAX NUMBER///////////
-function max(arr) {
-  var maxNumber = arr[0];
+// MAX NUMBER
+function max (arr) {
+  var maxNumber = arr[0]
   for (var i = 1; i < arr.length; i++) {
     if (arr[i] > maxNumber) {
-      maxNumber = arr[i];
+      maxNumber = arr[i]
     }
   }
-  console.log(maxNumber);
+  console.log(maxNumber)
 }
 
-max([1, NaN, null, undefined, 'abc', 3, 5, -3]);
+max([1, NaN, null, undefined, 'abc', 3, 5, -3, 30])
 
-
-
-
-/////////////MIN NUMBER//////////////
-function min(arr) {
-  var minNumber = arr[0];
+// MIN NUMBER
+function min (arr) {
+  var minNumber = arr[0]
   for (var i = 1; i < arr.length; i++) {
     if (arr[i] < minNumber) {
-      minNumber = arr[i];
+      minNumber = arr[i]
     }
   }
-  console.log(minNumber);
+  console.log(minNumber)
 }
 
-min([4, 12, 3, -3, null, NaN, undefined, 8, 123, 9, 22, 56]);
+min([4, 12, 3, -3, null, NaN, undefined, 8, 123, 9, 22, 56])
 
-
-
-
-//////////////SUM OF NUMBERS///////////
-function sum(arr) {
-  var sumOfNumbers = arr[0];
+// SUM OF NUMBERS
+function sum (arr) {
+  var sumOfNumbers = arr[0]
   for (var i = 1; i < arr.length; i++) {
-    if (Number.isInteger(arr[i])) {
-      sumOfNumbers += arr[i];
+    if (typeof (arr[i]) === 'number' && isNaN(arr[i]) !== true) {
+      sumOfNumbers += arr[i]
     }
   }
-  console.log(sumOfNumbers);
+  console.log(sumOfNumbers)
 }
 
-sum([1, 100, ,300, NaN, 'sdf', null, undefined, 5, -3]);
+sum([1, 100, 22, 300, NaN, 3.2, 'sdf', null, undefined, 5, -3])
